0dbbca22c2dcb787e4c9e966f0fcb7d7549f2fcd
andynnnfw <and345melo@gmail.com>
2026-08-27 10:45:14 -0300

    feito todo o sistema de login

--- app/src/main/java/com/example/mtecpamianderson/MainActivity.java
+++ app/src/main/java/com/example/mtecpamianderson/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    //foidd
+
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
